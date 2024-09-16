681bbc5ed1c5b4c79e073e82ab713b3f296de1b1
lex <you@example.com>
2024-09-17 00:49:37 +0800

    added vercel api

--- src/axios-client.js
+++ src/axios-client.js
@@ -1,29 +1,28 @@
-import axios from "axios";
+import axios from "axios"
 
 const axiosClient = axios.create({
-    baseURL: `http://localhost:8000/api`,
-});
+  baseURL: `https://laravelonly-copy-bi5wzqoc8-lexs-projects-4a50776e.vercel.app/api/api/`,
+})
 
 axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("ACCESS_TOKEN");
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
+  const token = localStorage.getItem("ACCESS_TOKEN")
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
 
 axiosClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        try {
-            const { response } = error;
-            if (response.status === 401)
-                localStorage.removeItem("ACCESS_TOKEN");
-        } catch (e) {
-            console.error(e);
-        }
-        throw error;
+  (response) => {
+    return response
+  },
+  (error) => {
+    try {
+      const { response } = error
+      if (response.status === 401) localStorage.removeItem("ACCESS_TOKEN")
+    } catch (e) {
+      console.error(e)
     }
-);
+    throw error
+  }
+)
 
-export default axiosClient;
+export default axiosClient
